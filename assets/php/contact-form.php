1b6372c6cd28ccd0b20f112767a17d3c7dcd012f
ymmourad <ymmourad@gmail.com>
2023-08-25 21:18:27 -0400

    release/1.0.0

---
+++ assets/php/contact-form.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "ymmourad@gmail.com"; 
+
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Subject: $subject\n\n";
+    $email_message .= "Message:\n$message";
+
+    // Send the email
+    mail($to, $subject, $email_message, $headers);
+
+    
+    header("Location: index.html"); 
+}?>
